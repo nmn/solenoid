7e029e6e04471ce0e301377665fc71635ff7ba3f
vikr01 <vik120@icloud.com>
2025-05-19 21:51:49 -0700

    drop use of infers

--- packages/server-runtime/src/index.ts
+++ packages/server-runtime/src/index.ts
@@ -2,20 +2,16 @@ import { type AnyFunction, type SolenoidFunctionConfig, SOLENOID_CUSTOM_KEY, SOL
 
 type SerializableFn<C extends any[] = any[], A extends AnyFunction = AnyFunction> = (...c: C)=>A;
 
-type ClosureOf<F extends SerializableFn> = F extends SerializableFn<infer C> ? C : never;
-type ArrowOf<F extends SerializableFn> = F extends SerializableFn<any[], infer A> ? A : never;
-
-
 type SerializableFnConf<F extends SerializableFn> = {
   fn: F,
 
-  closure: ()=>ClosureOf<F>,
+  closure: ()=>Parameters<F>,
   id: string,
 };
 
-export function serializableFn<T extends SerializableFn>({fn, closure, id}: SerializableFnConf<T>): ArrowOf<T> & SolenoidFunctionConfig<ClosureOf<T>> {
+export function serializableFn<T extends SerializableFn>({fn, closure, id}: SerializableFnConf<T>): ReturnType<T> & SolenoidFunctionConfig<Parameters<T>> {
   const closureVars = closure();
-  const fnWithClosuresFilled = fn(...closureVars) as ArrowOf<typeof fn>;
+  const fnWithClosuresFilled = fn(...closureVars) as ReturnType<typeof fn>;
 
   const config: SolenoidFunctionConfig<typeof closureVars> = {
     [SOLENOID_CUSTOM_KEY]: SOLENOID_OBJECT_TYPES.Function,
