1eb31f008a0b5c1ccd512f6a02c37fa23307b0f5
vikr01 <vik120@icloud.com>
2025-05-25 11:25:42 -0700

    reset, use templates for for-each

--- packages/custom-elements/src/for-each.ts
+++ packages/custom-elements/src/for-each.ts
@@ -48,11 +48,9 @@ export class ForEach extends HTMLElement {
   */
 	static observedAttributes = ["values"];
 
-	private values: () => unknown[];
-  private map: (val: unknown, index: number)=>HTMLElement | string;
+  hasRemovedTemplate: boolean = false;
 	isConnected = true;
 	private cleanUp: null | (() => void) = null;
-	private abortController: AbortController = new AbortController();
 
 	async connectedCallback() {
 		if (!this.isConnected) return;
@@ -60,38 +58,30 @@ export class ForEach extends HTMLElement {
 		if (!values) {
 			throw new Error("for-each must have a \"values\" attribute");
 		}
-    console.log('success');
-    // const map = this.getAttribute("map");
-    // if (!map) {
-		// 	throw new Error("for-each must have a \"map\" attribute");
-		// }
     
-		// this.isConnected = true;
-		// const [parsedValues, parsedMap] = await Promise.all([JSON_PARSE(values), JSON_PARSE(map)])
-
-		// if (parsedValues && typeof parsedValues === "function" && parsedMap && typeof parsedMap === 'function') {
-		// 	this.values = parsedValues;
-    //   this.map = parsedMap;
-
-		// 	requestAnimationFrame(() => this.render());
-		// } else {
-		// 	this.isConnected = false;
-		// }
+    requestAnimationFrame(()=>this.render());
 	}
 
+  initializeIndices() {
+    if (this.hasRemovedTemplate) {
+      return;
+    }
+
+    const childElement = this.children[0] as HTMLTemplateElement;
+    childElement.childNodes.forEach((node: ListItem, index)=>{
+      if (process.env.NODE_ENV === 'development' && !(node instanceof ListItem)) {
+        console.error('Every child of for-each must be a list-item');
+      }
+
+      node.__setIndex(index);
+    });
+
+    this.innerHTML = childElement.innerHTML;
+    this.hasRemovedTemplate = true;
+  }
+
 	render() {
-    // const stopScope = effectScope(()=>{
-    //   effect(()=>{
-    //     this.innerHTML = '';
-    //     const nodes = this.values().map((_, index)=>{
-    //       return this.map(computed(()=>this.values()[index]), index);
-    //     });
-
-    //     this.append(...nodes);
-    //   });
-    // });
-
-		// this.cleanUp = stopScope;
+    this.initializeIndices();
 	}
 
 	disconnectedCallback() {
@@ -104,7 +94,37 @@ customElements.define("for-each", ForEach);
 
 
 export class ListItem extends HTMLElement {
+  static observedAttributes = ["index"];
 
+  private cleanUp: null | (() => void) = null;
+  index: number;
+  isConnected = true;
+
+  async connectedCallback() {
+		if (!this.isConnected) return;
+		const index = this.getAttribute("index");
+    const indexNum = Number(index);
+		if (!index || isNaN(indexNum)) {
+			throw new Error("list-item must have a \"index\" attribute");
+		}
+    this.index = indexNum;
+  }
+
+  disconnectedCallback() {
+    this.cleanUp && this.cleanUp();
+		this.isConnected = false;
+  }
+
+  render() {
+  }
+
+  __setIndex(num: number) {
+    if (isNaN(num)) {
+      throw new Error(`invalid index set: ${num}`);
+    }
+
+    this.index = num;
+  }
 }
 
 customElements.define('list-item', ListItem);
