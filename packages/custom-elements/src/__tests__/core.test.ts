9947b59180494fdd5d6607e656bb429b0f53312c
vikr01 <vik120@icloud.com>
2025-05-27 00:06:25 -0700

    add back test for correct awaitGet

--- packages/custom-elements/src/__tests__/core.test.ts
+++ packages/custom-elements/src/__tests__/core.test.ts
@@ -25,23 +25,23 @@ describe("core", () => {
 			expect((window as any).signalStore).toBe(signalStore);
 		});
 
-		// 	test("awaitGet will wait until a signal is actually set", async () => {
-		// 		const key = "foo";
+		test("awaitGet will wait until a signal is actually set", async () => {
+			const key = "foo";
 
-		// 		const spy = vi.fn();
-		// 		const promise = signalStore.awaitGet(key);
-		// 		promise.then(spy);
+			const spy = vi.fn();
+			const promise = signalStore.awaitGet(key);
+			promise.then(spy);
 
-		// 		// wait for the next tick to ensure awaitGet gets a chance to run
-		// 		await delay(0);
-		// 		expect(spy).not.toHaveBeenCalled();
+			// wait for the next tick to ensure awaitGet gets a chance to run
+			await delay(0);
+			expect(spy).not.toHaveBeenCalled();
 
-		// 		const signal = createSignal(key, null);
-		// 		expect(signalStore.set(key, signal)).not.toBeNull();
+			const signal = createSignal(key, null);
+			expect(signalStore.set(key, signal)).not.toBeNull();
 
-		// 		await promise;
-		// 		expect(spy).toHaveBeenCalledExactlyOnceWith(signal);
-		// 	});
+			await promise;
+			expect(spy).toHaveBeenCalledExactlyOnceWith(signal);
+		});
 	});
 
 	describe("JSON_PARSE", () => {
