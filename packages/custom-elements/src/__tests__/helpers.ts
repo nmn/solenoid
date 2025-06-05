e3c00d870a4a9492c725dae9b55e758a71e42de9
vikr01 <vik120@icloud.com>
2025-06-05 22:47:45 +0530

    add tests for removal, methods

--- packages/custom-elements/src/__tests__/helpers.ts
+++ packages/custom-elements/src/__tests__/helpers.ts
@@ -4,3 +4,11 @@ import { expect } from "vitest";
 export function waitForElement(element: HTMLElement) {
 	return waitFor(() => expect(element.isConnected).toBe(true));
 }
+
+export function randomString() {
+	return Math.random().toString(36).slice(2);
+}
+
+export function waitForElementToBeRemoved(element: HTMLElement) {
+	return waitFor(() => expect(element.parentNode).toBeNull());
+}
--- packages/custom-elements/src/__tests__/let-signal.test.ts
+++ packages/custom-elements/src/__tests__/let-signal.test.ts
@@ -1,6 +1,10 @@
 // Instantiate custom elements
 import "..";
-import { waitForElement } from "./helpers";
+import {
+	randomString,
+	waitForElement,
+	waitForElementToBeRemoved,
+} from "./helpers";
 import { LetSignal } from "../let-signal";
 import { describe, expect, test, beforeEach } from "vitest";
 import { signalStore } from "../core";
@@ -12,8 +16,9 @@ describe("let-signal", () => {
 		document.body.innerHTML = "";
 	});
 
-	test("initializes a signal by the id passed to it", async () => {
-		const name = "124132512";
+	test("initializes a signal by the id passed to it, deletes it after unmount", async () => {
+		// initialization
+		const name = randomString();
 		const initialValue = 0;
 		const element = document.createElement("let-signal") as LetSignal<
 			typeof initialValue
@@ -23,15 +28,34 @@ describe("let-signal", () => {
 		element.setAttribute("name", name);
 		element.setAttribute("initial-value", JSON.stringify(initialValue));
 
+		// placement
 		document.body.append(element);
 		await waitForElement(element);
 
 		const signal = signalStore.get(name);
 		expect(signal).not.toBeNull();
 		expect(signal!()).toBe(initialValue);
+		expect(element.getSignal()).toBe(signal);
+
+		// removal
+		document.body.removeChild(element);
+		await waitForElementToBeRemoved(element);
+		expect(signalStore.get(name)).toBe(undefined);
 	});
 
-	// test('falls back to `null` if the initial value is not set or an empty string', async ()=>{
+	test("falls back to `null` if the initial value is not set or an empty string", async () => {
+		const name = randomString();
+		const element = document.createElement("let-signal") as LetSignal<null>;
+		expect(element).toBeInstanceOf(LetSignal);
 
-	// });
+		element.setAttribute("name", name);
+		element.setAttribute("initial-value", "");
+
+		document.body.append(element);
+		await waitForElement(element);
+
+		const signal = signalStore.get(name);
+		expect(signal).not.toBeNull();
+		expect(signal!()).toBe(null);
+	});
 });
