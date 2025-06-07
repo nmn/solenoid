af84241207a2e5c4faee13560601f8f855c98d0f
Vik R <vik120@icloud.com>
2025-06-07 13:03:15 +0530

    Add test for signal-text (#16)

--- packages/custom-elements/src/__tests__/helpers.ts
+++ packages/custom-elements/src/__tests__/helpers.ts
@@ -1,5 +1,15 @@
+import {
+	type AnyFunction,
+	type SolenoidSignalConfig,
+	type Signal,
+	SOLENOID_OBJECT_TYPES,
+	SOLENOID_CUSTOM_KEY,
+	type SolenoidFunctionConfig,
+} from "../utils/types";
 import { waitFor } from "@testing-library/dom";
-import { expect } from "vitest";
+import { expect, vi, type Mock } from "vitest";
+import { createSignal, signalStore } from "../core";
+import { effect, effectScope } from "alien-signals";
 
 export function waitForElement(element: HTMLElement) {
 	return waitFor(() => expect(element.isConnected).toBe(true));
@@ -11,4 +21,74 @@ export function randomString() {
 
 export function waitForElementToBeRemoved(element: HTMLElement) {
 	return waitFor(() => expect(element.parentNode).toBeNull());
+}
+
+export function createMockFunctionJSON<T extends () => unknown>(
+	fn: T,
+): [Mock<T>, string] {
+	let id: string;
+	do {
+		id = randomString();
+	} while (window.__FNS__[id] != null);
+
+	const config: SolenoidFunctionConfig = {
+		[SOLENOID_CUSTOM_KEY]: SOLENOID_OBJECT_TYPES.Function,
+		id,
+		module: id,
+		closure: [],
+		toJSON: undefined as unknown as SolenoidFunctionConfig["toJSON"],
+	};
+
+	const mockFn = vi.fn(fn);
+
+	window.__FNS__[id] = () => mockFn;
+
+	return [mockFn, JSON.stringify(config)];
+}
+
+export function createMockSignalJSON<T>(initialValue: T): [Signal<T>, string] {
+	let id: string;
+	do {
+		id = randomString();
+	} while (signalStore.get(id) != null);
+
+	const config: SolenoidSignalConfig = {
+		[SOLENOID_CUSTOM_KEY]: SOLENOID_OBJECT_TYPES.Signal,
+		id,
+	};
+
+	const signal = createSignal(id, initialValue);
+
+	signalStore.set(id, signal);
+
+	return [signal, JSON.stringify(config)];
+}
+
+export async function awaitRepaint() {
+	await new Promise((resolve) => requestAnimationFrame(resolve));
+}
+
+export async function awaitUpdateSignal<T>(
+	signal: Signal<T>,
+	nextVal: T,
+): Promise<void> {
+	const { promise, resolve } = Promise.withResolvers();
+
+	let initialized = false;
+	const unsub = effectScope(() => {
+		effect(() => {
+			signal();
+			// The first execution will be for subscription sake.
+			if (initialized) {
+				resolve();
+				return;
+			}
+			initialized = true;
+		});
+	});
+
+	signal(nextVal);
+	await promise;
+	unsub();
+	await awaitRepaint();
 }
---
+++ packages/custom-elements/src/__tests__/signal-text.test.ts
@@ -0,0 +1,82 @@
+// Instantiate custom elements
+import "..";
+import {
+	awaitRepaint,
+	awaitUpdateSignal,
+	createMockFunctionJSON,
+	createMockSignalJSON,
+} from "./helpers";
+import { SignalText } from "../signal-html";
+import { describe, expect, test, beforeEach, vi } from "vitest";
+import { signalStore } from "../core";
+
+describe("signal-text", () => {
+	beforeEach(() => {
+		window.__FNS__ = {};
+		signalStore.clear();
+		document.body.innerHTML = "";
+	});
+
+	test('document.createElement("signal-text") is a SignalText instance', () => {
+		const element = document.createElement("signal-text");
+		expect(element.constructor).toBe(SignalText);
+	});
+
+	test("properly subscribes to a signal from the id in the JSON parameter", async () => {
+		const element = new SignalText();
+
+		const initialText = "foobar";
+		const [signal, configJSON] = createMockSignalJSON(initialText);
+		element.setAttribute("value", configJSON);
+		await element.connectedCallback();
+		await awaitRepaint();
+
+		expect(element.innerText).toEqual(initialText);
+		expect((element as any).value).toBe(signal);
+
+		const nextText = "testing for update";
+		await awaitUpdateSignal(signal, nextText);
+
+		expect(signal()).toBe(nextText);
+		expect(element.innerText).toEqual(nextText);
+	});
+
+	test("will still work on non-signal JSONs of functions", async () => {
+		const element = new SignalText();
+
+		const initialText = "foobar";
+		const [signalLike, configJSON] = createMockFunctionJSON(
+			(_?: string) => initialText,
+		);
+		element.setAttribute("value", configJSON);
+
+		await element.connectedCallback();
+		await awaitRepaint();
+		signalLike.mockClear();
+
+		expect(element.innerText).toBe(initialText);
+		await (element as any).value();
+		expect(signalLike).toHaveBeenCalledTimes(1);
+	});
+
+	test("unsubscribes to a signal after disconnection", async () => {
+		const element = new SignalText();
+
+		const initialText = "foobar";
+		const [signal, configJSON] = createMockSignalJSON(initialText);
+		element.setAttribute("value", configJSON);
+		await element.connectedCallback();
+		await awaitRepaint();
+
+		expect(element.innerText).toEqual(initialText);
+		expect((element as any).value).toBe(signal);
+
+		element.disconnectedCallback();
+
+		const nextText = "testing for update";
+		await awaitUpdateSignal(signal, nextText);
+
+		expect(signal()).toBe(nextText);
+		expect(element.innerText).toEqual(initialText);
+	});
+});
