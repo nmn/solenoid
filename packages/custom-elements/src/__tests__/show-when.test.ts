74ab86bf2e2feb8ded8167b00eadd6d21a181f46
vikr01 <vik120@icloud.com>
2025-06-08 23:29:28 +0530

    add test for show-when

---
+++ packages/custom-elements/src/__tests__/show-when.test.ts
@@ -0,0 +1,47 @@
+// Instantiate custom elements
+import "..";
+import {
+	awaitRepaint,
+	awaitUpdateSignal,
+	createMockSignalJSON,
+} from "./helpers";
+import { ShowWhen } from "../signal-html";
+import { describe, expect, test, beforeEach } from "vitest";
+import { signalStore } from "../core";
+
+describe("show-when", () => {
+	beforeEach(() => {
+		window.__FNS__ = {};
+		signalStore.clear();
+		document.body.innerHTML = "";
+	});
+
+	test('document.createElement("show-when") is a ShowWhen instance', () => {
+		const element = document.createElement("show-when");
+		expect(element.constructor).toBe(ShowWhen);
+	});
+
+	test("properly subscribes to a signal from the id in the JSON parameter", async () => {
+		const element = document.createElement("show-when") as ShowWhen;
+
+		const childElement = document.createElement("div");
+		childElement.append("signal test for show when");
+
+		const [signal, configJSON] = createMockSignalJSON(true);
+		element.setAttribute("condition", configJSON);
+		element.append(childElement);
+
+		await element.connectedCallback();
+		await awaitRepaint();
+
+		expect(Array.from(element.children)).toContainEqual(childElement);
+
+		await awaitUpdateSignal(signal, false);
+
+		expect(Array.from(element.children)).not.toContainEqual(childElement);
+
+		await awaitUpdateSignal(signal, true);
+
+		expect(Array.from(element.children)).toContainEqual(childElement);
+	});
+});
